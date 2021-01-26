dfc67d6969666117b1afda86e58d05df84748178
Flare-k <rokkyw@naver.com>
2021-01-27 00:20:15 +0900

    first commit

---
+++ src/test/java/com/apple/mySpringHome/MySpringHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.mySpringHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

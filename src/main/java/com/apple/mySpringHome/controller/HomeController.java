f29345ae6d9e87cd42447087d120cb0e16c6621d
Flare-k <rokkyw@naver.com>
2021-01-27 01:59:44 +0900

    홈, 게시판 화면 구성

---
+++ src/main/java/com/apple/mySpringHome/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.apple.mySpringHome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping // '/'
+    public String index() {
+        return "index";
+    }
+}

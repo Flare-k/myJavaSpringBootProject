1a3fd8738f59e6abc889fc79caf9a54fbe581868
Flare-k <rokkyw@naver.com>
2021-01-27 00:29:15 +0900

    test commit

--- src/main/java/com/apple/mySpringHome/MySpringHomeApplication.java
+++ src/main/java/com/apple/mySpringHome/MySpringHomeApplication.java
@@ -2,7 +2,7 @@ package com.apple.mySpringHome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// main
 @SpringBootApplication
 public class MySpringHomeApplication {
 
